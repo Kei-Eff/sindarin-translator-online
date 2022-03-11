d52d0784a93b9bd885b78ad7160635f92775aaf0
Kei-Eff <karl.alberto@hotmail.com>
2022-03-11 17:45:49 +1100

    Added more unittests

--- test_translation_cache.py
+++ test_translation_cache.py
@@ -3,13 +3,14 @@ import boto3
 from translation_cache import Cache
 from moto import mock_dynamodb2
 
+
+@mock_dynamodb2
 class TestTranslationCache(unittest.TestCase):
 
-    @mock_dynamodb2
-    def test_cache_hit_returns_item(self):
-        # Arrange
-        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
-        table = dynamodb.create_table(
+    # Setup test DynamoDB table
+    def setUp(self) -> None:
+        self.dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
+        self.table = self.dynamodb.create_table(
             TableName='SindarinCache',
             KeySchema=[
                 {'AttributeName': 'text',
@@ -24,7 +25,12 @@ class TestTranslationCache(unittest.TestCase):
                 'WriteCapacityUnits': 5
             }
         )
-        table.put_item(
+
+    def test_cache_hit_returns_item(self):
+        """Given a key that exists in the cache, cache should return an item.
+        """
+        # Arrange
+        self.table.put_item(
                 Item={
                     'text': 'friend',
                     'translation': 'mellon'
@@ -32,16 +38,42 @@ class TestTranslationCache(unittest.TestCase):
             )
 
         # Act
-        system_under_test = Cache(dynamodb)
+        system_under_test = Cache(self.dynamodb)
         result = system_under_test.get_item('friend')
 
         # Assert
         assert result['translation'] == 'mellon'
 
-    @mock_dynamodb2
-    def test_cache_hit_case_insensitive(self):
-        pass
 
-    @mock_dynamodb2
+    def test_cache_hit_case_insensitive(self):
+        """Given a key that exists in the cache, in lowercase; and message is sent in uppercase/titlecase, cache should return an item regardless of case.
+        """
+        # Arrange
+        self.table.put_item(
+                Item={
+                    'text': 'friend',
+                    'translation': 'mellon'
+                }
+            )
+
+        # Act
+        system_under_test = Cache(self.dynamodb)
+        result = system_under_test.get_item('FrIeNd')
+
+        # Assert
+        assert result['translation'] == 'mellon'
+
+
     def test_cache_miss_returns_none(self):
-        pass
+        """Given a key that does not exist in the cache, cache should return None.
+        """
+        # Arrange
+        
+        # Empty table
+
+        # Act
+        system_under_test = Cache(self.dynamodb)
+        result = system_under_test.get_item('friend')
+
+        # Assert
+        assert result == None
